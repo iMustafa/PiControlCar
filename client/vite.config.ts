27d359a3c9aa90e3adc8d57aced9d4faad69b56c
Mustafa Ahmed <iMustafa97@outlook.com>
2025-09-02 15:38:16 +0300

    Update

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,8 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     "allowedHosts": ["snowball.local"],
-  },
-  build: {
+  },build: {
     outDir: 'dist',
     emptyOutDir: true,
   }
